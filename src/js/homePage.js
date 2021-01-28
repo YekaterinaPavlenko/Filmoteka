755fd056960f81234d642dcf89b924528b4f3d2b
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-01-28 22:54:32 +0200

    Исправляет ошибки в загрузке популярных фильмов

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,46 +1,44 @@
-import refs from './js/refs.js';
+import refs from './refs';
 // import notifications from './js/notifications.js'; //Подключить файл с нотификашкой
 import updateMarcup from './updateMarkupGallery.js';
 import LoadMoreBtn from './loadMoreBtn.js';
-import MovieApiService from './js/apiService.js';
+import MovieApiService from './apiService.js';
 
 const loadMoreBtn = new LoadMoreBtn({
-  // Создаю экземпляр кнопки загрузить еще
-  selector: '.js-load_more',
+  selector: '.js-load_more', // Создаю экземпляр кнопки загрузить еще
   hidden: true,
 });
 
 // console.log(loadMoreBtn);
 const movieApiService = new MovieApiService(); //Создаю экземпляр класса поиска фильмов
 
-refs.homeBtn.addEventListener('click', sendToHomePage); // слушатель на кнопке НОМЕ- отправляет на основную(первую) стр.
-refs.logoBtn.addEventListener('click', sendToHomePage); // слушатель на кнопке Filmoteka ^ делает то же самое
+// refs.homeBtn.addEventListener('click', sendToHomePage); // слушатель на кнопке НОМЕ- отправляет на основную(первую) стр.
+// refs.logoBtn.addEventListener('click', sendToHomePage); // слушатель на кнопке Filmoteka ^ делает то же самое
 loadMoreBtn.refs.button.addEventListener('click', fetchPopMovies);
 
 fetchPopMovies(); //Запрос и отрисовка главной страницы при  первой загрузке
 loadMoreBtn.show(); // показывает кнопку "загрузить еще"
 
 function fetchPopMovies() {
-  // сама ф-ция  запроса и отрисовки результата запроса
-  // console.log(document.body.scrollHeight);
+  // console.log(document.body.scrollHeight);   // сама ф-ция  запроса и отрисовки результата запроса
   loadMoreBtn.enable();
   movieApiService
     .fetchPopularMovies()
     .then(results => {
       //   console.log(movieApiService.page);
-      //   console.log(results);
-      if (results.length === 0) {
-        // notifications.notFound(); //Вместо нотификашки подключить отбражение нотификации под формой 'это все фильмы, которые мы нашли'
-      }
+      console.log(results);
+      // if (results.length === 0) {
+      //   // notifications.notFound(); //Вместо нотификашки подключить отбражение нотификации под формой 'это все фильмы, которые мы нашли'
+      // }
       loadMoreBtn.show();
       loadMoreBtn.disable();
       updateMarcup(results);
       loadMoreBtn.enable();
 
-      refs.gallery.addEventListener('click', createModalImg); // Любомир вместо createModalImg должен поставить свою ф-цию открытия модалки.
+      // refs.gallery.addEventListener('click', createModalImg); // Любомир вместо createModalImg должен поставить свою ф-цию открытия модалки.
     })
     .catch(error => {
-      notifications.errorRequest;
+      // notifications.errorRequest;
       loadMoreBtn.hide();
     });
 }
--- src/js/updateMarkupGallery.js
+++ src/js/updateMarkupGallery.js
@@ -1,3 +1,4 @@
+import refs from './refs.js';
 import galleryTpl from '../templates/cardMainPage.hbs';
 export default function updateMarcup(results) {
   refs.gallery.insertAdjacentHTML('beforeend', galleryTpl(results));

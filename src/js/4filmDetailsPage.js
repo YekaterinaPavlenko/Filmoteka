55b0faa68266fdd690bb04636b9ebde987ff1b36
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2021-02-01 02:12:03 +0200

    init localStorage

--- src/js/4filmDetailsPage.js
+++ src/js/4filmDetailsPage.js
@@ -1,21 +1,25 @@
 import refs from './refs';
 import modalTpl from '../templates/detailsPage.hbs';
 import MovieApiService from './apiService.js';
+import { parse } from 'handlebars';
 
 // const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'c2406e33bae3c04a8fdebb618c81ede7';
 
-console.log(refs.openModal);
-console.log(refs.modal);
+// console.log(refs.openModal);
+// console.log(refs.modal);
 
 refs.openModal.addEventListener('click', openModal);
 
+let id;
+
 function openModal(event) {
-  console.log(event.target);
+  // console.log(event.target);
   if (event.target.nodeName !== 'IMG') {
     return;
   }
-  let id = 550;
+  id = event.target.dataset.id;
+  // console.log(id);
   refs.modalBox.classList.remove('is-hidden');
   fetchFilm(id);
   refs.openModal.removeEventListener('click', openModal);
@@ -33,7 +37,7 @@ function closeModal(e) {
     window.removeEventListener('keydown', closeModal);
     refs.backdrop.removeEventListener('click', closeModal);
     refs.openModal.addEventListener('click', openModal);
-    console.log('close modal window');
+    // console.log('close modal window');
   }
 }
 
@@ -45,7 +49,7 @@ function fetchFilm(id) {
     .then(response => response.json())
 
     .then(movie => {
-      console.log(movie);
+      // console.log(movie);
       appendMarkup(movie);
     });
 }
@@ -53,4 +57,64 @@ function fetchFilm(id) {
 function appendMarkup(movie) {
   const modalContent = modalTpl(movie);
   refs.modalContent.insertAdjacentHTML('beforeend', modalContent);
+
+  addFilmsToLibrary();
+}
+
+/*==============LocalStorage=============*/
+
+function addFilmsToLibrary() {
+  const watchedBtn = document.querySelector('.watched-btn');
+  const queueBtn = document.querySelector('.queue-btn');
+
+  watchedBtn.addEventListener('click', handleBtnWatched);
+  queueBtn.addEventListener('click', handleBtnQueue);
+
+  function handleBtnWatched() {
+    watchedBtn.textContent = 'Delete from watched';
+
+    addToWatched(id);
+  }
+
+  function handleBtnQueue() {
+    queueBtn.textContent = 'Delete from watched';
+
+    addToQueue(id);
+  }
+}
+
+/*функции проверяющие есть ли в массивах текущий фильм, если нет - добавляет его в локальное хранилище*/
+
+let filmsWatched = [];
+
+function addToWatched() {
+  let localStorageData = localStorage.getItem('filmsWatched');
+  if (localStorageData) {
+    filmsWatched = [...JSON.parse(localStorageData)];
+  }
+  let currentIdFilm = id;
+
+  let filmId = filmsWatched.find(el => el === currentIdFilm);
+  if (filmId === currentIdFilm) {
+    return;
+  }
+  filmsWatched.push(id);
+  localStorage.setItem('filmsWatched', JSON.stringify(filmsWatched));
+}
+
+let filmsQueue = [];
+
+function addToQueue() {
+  let localStorageData = localStorage.getItem('filmsQueue');
+  if (localStorageData) {
+    filmsQueue = [...JSON.parse(localStorageData)];
+  }
+  let currentIdFilm = id;
+
+  let filmId = filmsQueue.find(el => el === currentIdFilm);
+  if (filmId === currentIdFilm) {
+    return;
+  }
+  filmsQueue.push(id);
+  localStorage.setItem('filmsQueue', JSON.stringify(filmsQueue));
 }

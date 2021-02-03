1ff5aa8b9d2b40188266a368b5381538abd8b1c6
Yevheniia123 <zenjunja91@gmail.com>
2021-02-03 14:42:35 +0100

    init library

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -14,6 +14,7 @@ function mainPage() {
   refs.headerRef.classList.remove('bcg-libr');
   refs.libraryBtnRef.classList.remove('current');
   refs.homeBtn.classList.add('current');
+  refs.gallery.classList.remove('gallery-bgr');
 }
 function library() {
   refs.searchForm.classList.add('is-hidden');
@@ -22,6 +23,7 @@ function library() {
   refs.headerRef.classList.add('bcg-libr');
   refs.libraryBtnRef.classList.add('current');
   refs.homeBtn.classList.remove('current');
+  // gallery - bgr;
 }
 function queue() {
   refs.watchedBtnRef.classList.remove('orange');
@@ -39,11 +41,14 @@ refs.logoBtn.addEventListener('click', mainPage);
 
 refs.libraryBtnRef.addEventListener('click', showWatched);
 refs.watchedBtnRef.addEventListener('click', showWatched);
-
 refs.queueBtnRef.addEventListener('click', event => {
   cleanGallery();
   const localStr = localStorage.getItem('filmsQueue');
   const parse = JSON.parse(localStr);
+  if (localStr === null) {
+    refs.gallery.classList.add('gallery-bgr');
+    refs.gallery.insertAdjacentHTML('beforeend', addText());
+  } else refs.gallery.classList.remove('gallery-bgr');
 
   for (let item of parse) {
     fetchItem(item).then(movie => {
@@ -67,7 +72,10 @@ function showWatched() {
   cleanGallery();
   const localStr = localStorage.getItem('filmsWatched');
   const parse = JSON.parse(localStr);
-
+  if (localStr === null) {
+    refs.gallery.classList.add('gallery-bgr');
+    refs.gallery.insertAdjacentHTML('beforeend', addText());
+  } else refs.gallery.classList.remove('gallery-bgr');
   for (let item of parse) {
     fetchItem(item).then(movie => {
       renderItems(movie);
@@ -77,4 +85,7 @@ function showWatched() {
 
 function cleanGallery() {
   refs.gallery.innerHTML = '';
+}
+function addText() {
+  return `<p class="empty-library">You have not chosen a movie<br/><a href="#" class="choose">Choose now</a></p>`;
 }

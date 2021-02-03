024c7ec78a83cbb000a5b871d5e12ffed107108a
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-02-03 17:10:13 +0200

    добавляет часть пагинации2

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -19,19 +19,36 @@ const eightBtn = document.getElementById('pag8');
 const ninthBtn = document.getElementById('pag9');
 
 refs.pagingList.addEventListener('click', updateMarkupByPages);
+refs.pagingList.addEventListener('click', setNumberOfPageBtn);
+refs.pagingList.addEventListener('click', updateMarkupByPages);
 
+function setNumberOfPageBtn(event) {
+  event.preventDefault();
+  if (event.target.textContent == 1) {
+    console.log(firstBtn);
+    firstBtn.textContent = 1;
+    secondBtn.textContent = 2;
+    thirdBtn.textContent = 3;
+    fourthBtn.textContent = 4;
+    fifthBtn.textContent = 5;
+    sixthBtn.textContent = 6;
+    seventhBtn.textContent = 7;
+    eightBtn.textContent = '';
+    eightBtn.classList.add('three - dots');
+  }
+}
 function updateMarkupByPages(event) {
   event.preventDefault();
   //   console.log(event.currentTarget);
   //   console.log(event.target.classList);
-  console.dir(event.currentTarget.children);
+  // console.dir(event.currentTarget.children);
   if (event.target.classList.value.includes('js-page-number')) {
     movieApiService.page = +event.target.textContent;
     // console.log(event.target.classList.value);
-    console.log(event.currentTarget.children);
+    // console.log(event.currentTarget.children);
 
     // pagingItems.classList.value.remove('js-current-page');
     event.target.classList.add('js-current-page');
-    console.log(movieApiService.page);
+    // console.log(movieApiService.page);
   }
 }
